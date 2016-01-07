174e9969c93d2e05122bf0f8fdb40c2909859ec1
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-01-07 11:41:40 +0100

    Update console.snippet.001.js

--- console.snippet.001.js
+++ console.snippet.001.js
@@ -1,27 +1,35 @@
-//
 'use strict';
 (function($window){
  function Capitalized(x){var s=x;return s.charAt(0).toUpperCase()+s.slice(1);}
 
  var w=$window, $console=w.console;
 
- ['log','group','groupEnd','groupCollapsed','clear'].forEach(function(x){
-	var p=x,o=$console,k='log';
-	w[p===k?k:k+Capitalized(p)]=function(){
+ ['dir','log','group','groupEnd','groupCollapsed','clear'].forEach(function(x){
+	var p=x,o=$console;
+	w[p.length===3?p:'log'+Capitalized(p)]=function(){
 	 o[p].apply(o,arguments);
 	};
  });
 
-
 })(window);
-//
-
+/*
+dir({});
 log(1);
-
 logGroup(2);
 log(21);
 logGroupEnd();
-
 logGroupCollapsed(3);
 log(31);
 logGroupEnd();
+*/
+
+(function($window){
+
+ var w=$window,
+ Call=w.Function.call,
+ Slice=Call.bind(w.Array.prototype.slice),
+ $document=w.document,
+ end;
+
+
+})(window);
