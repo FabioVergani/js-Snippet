ab1390fa86aabd226501050f9de9977c37e5e2ae
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-10-12 12:47:59 +0200

    Update url filename.js

--- url/url filename.js
+++ url/url filename.js
@@ -1,2 +1,15 @@
-var s=window.location.pathname;
-s=s.substring(s.lastIndexOf('/')+1);//filename
+var l=window.location,s=l.pathname;
+if(s.length>1){
+ l='/';
+ if(s.endsWith(l)){
+	//nofile
+ }else{
+	 if(l=s.lastIndexOf(l)){
+		s=s.substring(l+1);//filenameWithExtensionOrAncor
+		if(l=s.lastIndexOf('.')){
+			s=s.substring(0,l);//filenameOnly
+			console.log(s);
+		};
+	 };
+ };
+};
