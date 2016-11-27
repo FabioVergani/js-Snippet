9654de7516428824c51bc2e48ca423f1078d766c
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-11-27 23:23:27 +0100

    Update _createClass.js

--- class/_createClass.js
+++ class/_createClass.js
@@ -1,46 +1,61 @@
-function _classCallCheck(a,b){// instance, Constructor
-  if(a instanceof b){
-	//console.info('%O'+a+'is instance of%O'+b);
-  }else{
-    throw new TypeError('Called a class as a function...');
-  };
-}
-
-
-function defineProperties(o,p){// dest, props
- const m=p;
- var i=m.length;
- if(i>0){
-  const f=Object.defineProperty;
-  let e;
-  while(i>0){
-	e=m[--i];
-	e.configurable=true;
-	e.enumerable=(e.enumerable||false);
-	if('value' in e){e.writable=true;};
-	f(o,e.key,e);
+console.clear();
+//
+const O=Object,rawObj=O.create.bind(null,null);
+O.defineProperty(rawObj,'O',{value:Object});
+//
+function define(a,b){//obj,keys
+ const t=a||null,r=rawObj,x=r();
+ if(x.target=t){
+  const q=b,O=r.O,k=O.keys(q);
+  let i=k.length;
+  if(x.flag=i>0){
+   const d=r();
+   while(i>0){
+	const o=r();
+	let p=k[--i],e=q[p];
+	d[p]=o;
+	if(e){
+	 let v=(o[p='value']=e[p])!==undefined;
+	 o[p='writable']=v=v?p in e?e[p]:true:true;
+	 o[p='enumerable']=p in e?e[p]:true;
+	 o[p='configurable']=p in e?e[p]:v;
+	};
+   }; 
+   O.defineProperties(t,d);
   };
  };
+ return x;
 }
-
-
-function _createClass(a,b,c){// Constructor, protoProps, staticProps
- const f=defineProperties;
- if(b){f(a.prototype,b);};
- if(c){f(a,c);}
- return a;
+//
+function createClass(a){//props
+ const o=define(rawObj(),a);
+ return o.flag?o.target:null
 } 
 
+//-------------------------------------
 
-var laMiaClasse = _createClass(Function(),[{
-	key:'pippo',
-	value:function(){alert(222)}
-},{
-	key:'pluto',
-	value:2
-}]);
+var laMiaClasse=createClass({
+	pippo:{value:function(){
+		console.info(this);
+		alert(++this.counter);
+	}},
+	pluto:{value:2}
+});
+//
 
-var istanzaDellaMiaClasse=new laMiaClasse;
+var istanza1DellaMiaClasse=O.create(laMiaClasse);
+istanza1DellaMiaClasse.counter=1;
+var istanza2DellaMiaClasse=O.create(laMiaClasse);
+istanza2DellaMiaClasse.counter=2;
+
+istanza1DellaMiaClasse.pippo();//2
+istanza2DellaMiaClasse.pippo();
+
+istanza1DellaMiaClasse.pippo();//3
+istanza2DellaMiaClasse.pippo();
 
 
-istanzaDellaMiaClasse.pippo();
+istanza1DellaMiaClasse.pippo=function(){alert('kk');}
+istanza1DellaMiaClasse.pippo();
+istanza2DellaMiaClasse.pippo();
+
