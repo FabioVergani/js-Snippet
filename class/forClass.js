1932fe8050b7f38043107c1c7382c540a85038dc
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-11-29 17:29:31 +0100

    Update forClass.js

--- class/forClass.js
+++ class/forClass.js
@@ -7,13 +7,22 @@ aRawObj=aBindedCall(Create,null,null);
 //----------------------------------------------
 var myClass=aRawObj({
   property1:{value:true,writable:true},
-  property2:{value:"Hello"}
+  property2:{value:"Hello"},
+  method1:{value:function(){
+	  console.log(this.property1);
+	  console.log(++this.counter);
+  }}
+  
 });
 //
 var istanza1=Create(myClass);
 istanza1.counter=1;
+istanza1.method1();
+
 var istanza2=Create(myClass);
 istanza2.counter=2;
+istanza2.method1();
+
 
 console.dir(istanza1);
 console.dir(istanza2);
