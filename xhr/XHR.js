e47dd64421deee704b06ddbcc0d1e515a3350b27
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-01-15 14:46:05 +0100

    Update XHR.js

--- xhr/XHR.js
+++ xhr/XHR.js
@@ -4,3 +4,6 @@
 	o.onprogress=function(){f(o.readyState===4,o.status===200,o);};//f(done,success,request)
 	o.send(e);
  }
+
+
+//function XHR(a,b,c='GET',d=200,e=null){const f=b, o=new w.XMLHttpRequest;o.open(c,a,true);o.onload=function(){f(o.status===200,o);};o.send(e);}
