31c001b89d9106c88ac618389807da1e073d1af7
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-01-15 20:04:03 +0100

    Update XHR.js

--- xhr/XHR.js
+++ xhr/XHR.js
@@ -1,4 +1,15 @@
- function XHR(a,b,c='GET',d=200,e=null){//url,handle-progress
+ 		function XHR(a,b){const o=new w.XMLHttpRequest;o.open(a,b,true);return o;}
+		function getFileAsText(a,b,c=null){const o=XHR('GET',a);o.onload=b;o.send(c);}
+
+
+
+
+
+
+
+
+
+function XHR(a,b,c='GET',d=200,e=null){//url,handle-progress
 	const f=b, o=new XMLHttpRequest;
 	o.open(c,a,true);//method,url,async
 	o.onprogress=function(){f(o.readyState===4,o.status===200,o);};//f(done,success,request)
