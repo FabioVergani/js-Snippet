8a9a8e79d1996e5c7b31015d9e0893a2a417ef91
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-03-26 20:29:29 +0200

    Create langcountry.js

---
+++ lang/langcountry.js
@@ -0,0 +1,123 @@
+//last two char of lang-code
+const langcountry={
+AE:[24.34,52.81],//Emirati Arabi Uniti - UAE
+af:[8.6,25],//Africa
+AR:[-36.98,-81.63],//Argentina
+AT:[47.67,11.1],//Austria
+AU:[24.98,115.18],//Australia
+BE:[50.49,3.35],//Belgio
+be:[53.63,23.49],//Bielorussia
+bg:[42.70,23.23],//Bulgaria
+BH:[25.95,50.32],//Bahrain
+BO:[-16.22,-68],//Bolivia
+BR:[-13.49,-69.78],//Brasile
+BZ:[17.19,-89.48],//Belize
+ca:[41.68,0.62],//Catalogna
+CA:[50.87,-130.16],//Canada
+CH:[46.80,7.1],//Svizzera
+CL:[-35.32,-89.11],//Cile
+CN:[34.52,86.04],//Cina - PRC
+CO:[4.08,-81.98],//Colombia
+CR:[9.63,-85.37],//Costa Rica
+cs:[49.78,13.23],//Repubblica Ceca
+da:[56.13,8.19],//Danimarca
+de:[51.08,5.96],//Germania
+DO:[18.7,-71.28],//Repubblica Dominicana
+DZ:[27.75,-7.35],//Algeria
+EC:[-1.78,-80.38],//Ecuador
+EG:[26.77,26.3],//Egitto
+el:[38.19,19.31],//Grecia
+en:[54.23,-6.72],//Inghilterra, Regno Unito
+es:[0.13,-8.2],//Spagna
+et:[58.58,22.74],//Estonia
+eu:[42.96,-3.14],//Paesi Baschi
+fa:[38.84,69],//Tagikistan
+fi:[63.50,0.21],//Norvegia
+FI:[64.65,17.13],//Finlandia
+fo:[61.41,-4.22],//Isole Faroe
+fr:[46.13,-2.27],//Francia
+GB:[54.23,-6.72],//Gran Bretagna
+gd:[57.67,-9.17],//Scozia
+GT:[15.77,-92.47],//Guatemala
+he:[31.39,32.83],//Israele
+hi:[20.19,64.46],//India
+HK:[22.35,114],//Hong Kong
+HN:[14.74,-88.48],//Honduras
+hr:[44.45,14.22],//Croazia
+hu:[47.14,17.26],//Ungheria
+id:[2.37,99.57],//Indonesia
+IE:[53.40,-10.57],//Irlanda
+IQ:[33.14,39.22],//Iraq
+is:[64.86,-23.49],//Islanda
+it:[40.95,3.58],//Italia
+ja:[34.46,125.36],//Giappone
+ji:[48.57,131.63],//Oblast
+JM:[18.11,-77.83],//Giamaica
+JO:[31.26,34.87],//Giordania
+ko:[37.93,123.17],//Corea
+KW:[29.31,46.93],//Kuwait
+LB:[33.87,34.74],//Libano
+LI:[47.15,9.41],//Liechtenstein
+lt:[55.15,21.65],//Lituania
+LU:[49.81,5.57],//Lussemburgo
+lv:[56.86,22.36],//Lettonia
+LY:[26.07,8.24],//Libia
+MA:[31.72,-11.57],//Marocco
+mk:[41.11,21.11],//Macedonia - FYROM
+MO:[46.96,26.14],//Moldavia
+mt:[35.94,14.23],//Malta
+MX:[23.38,-111.56],//Messico
+NI:[12.86,-87.38],//Nicaragua
+nl:[52.19,3.03],//Paesi Bassi
+no:[63.5,0.21],//Norvegia
+NZ:[43,163.46],//Nuova Zelanda
+OM:[21.47,51.42],//Oman
+PA:[8.42,-82.35],//Panama
+PE:[-9.06,-84.03],//Perù
+pl:[51.84,14.64],//Polonia
+PR:[18.19,-66.91],//Puerto Rico
+pt:[39.53,-10.09],//Portogallo
+PY:[-23.36,-62.94],//Paraguay
+QA:[25.32,50.63],//Qatar
+rm:[46.65,10.86],//Sud Tirolo
+ro:[45.92,22.76],//Romania
+ru:[49.78,68.72],//Russia
+SA:[23.87,36.07],//Arabia Saudita 
+sb:[49.43,15.5],//Lusazia
+SG:[1.31,103.7],//Singapore
+sk:[48.65,17.45],//Slovacchia
+sl:[46.14,13.87],//Slovenia
+sq:[41,20],//Albania
+sr:[44.19,18.67],//Serbia
+SV:[13.80,-89.46],//El Salvador
+sv:[61.91,8.62],//Svezia
+sx:[-29.61,27.11],//Lesotho
+SY:[34.79,36.80],//Siria
+sz:[67.73,20.8],//Lapponia
+th:[12.9,92.44],//Tailandia
+tn:[-22.27,20.19],//Botswana
+TN:[33.72,5.06],//Tunisia
+tr:[38.87,30.75],//Turchia
+ts:[-18.43,26.49],//Mozambico
+TT:[10.44,-61.7],//Trinidad e Tobago
+TW:[23.59,119.89],//Taiwan
+uk:[48.3,26.69],//Ucraina
+ur:[30.1,60.33],//Pakistan
+US:[36.24,-113.72],//Stati Uniti - USA
+UY:[-32.53,-58.05],//Uruguay
+ve:[-18.99,26.9],//Zimbabwe
+VE:[6.64,-71.11],//Venezuela
+vi:[15.73,96.76],//Vietnam
+xh:[-0.64,10.39],//Congo
+YE:[15.51,43.67],//Yemen
+ZA:[28.39,20.17],//Sudafrica
+zu:[-13.19,29.79]//Malawi
+}
+console.dir(langcountry['ca'])
+
+
+
+
+
+
+
