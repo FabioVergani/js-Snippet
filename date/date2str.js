51771bd96365b22ed8789927b095bb8f3dd76943
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-03-31 15:32:14 +0200

    Create date2str.js

---
+++ date/date2str.js
@@ -0,0 +1,23 @@
+format = function date2str(x, y) {
+    var z = {
+        M: x.getMonth() + 1,
+        d: x.getDate(),
+        h: x.getHours(),
+        m: x.getMinutes(),
+        s: x.getSeconds()
+    };
+    y = y.replace(/(M+|d+|h+|m+|s+)/g, function(v) {
+        return ((v.length > 1 ? "0" : "") + eval('z.' + v.slice(-1))).slice(-2)
+    });
+
+    return y.replace(/(y+)/g, function(v) {
+        return x.getFullYear().toString().slice(-v.length)
+    });
+}
+
+/*
+result:
+
+format(new Date('Sun May 11,2014'), 'yyyy-MM-dd')
+"2014-05-11
+*/
