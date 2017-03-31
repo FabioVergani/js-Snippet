c4c11055c398675549e3df3e58074fe157a9a5b6
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-03-31 15:36:41 +0200

    Update date2str.js

--- date/date2str.js
+++ date/date2str.js
@@ -20,4 +20,26 @@ result:
 
 format(new Date('Sun May 11,2014'), 'yyyy-MM-dd')
 "2014-05-11
+
+test
+
+(function(x, y) {
+    var z = {
+        M: x.getMonth() + 1,
+        d: x.getDate(),
+        h: x.getHours(),
+        m: x.getMinutes(),
+        s: x.getSeconds()
+    };
+    y = y.replace(/(M+|d+|h+|m+|s+)/g, function(v) {
+        return ((v.length > 1 ? "0" : "") + eval('z.' + v.slice(-1))).slice(-2)
+    });
+
+    return y.replace(/(y+)/g, function(v) {
+        return x.getFullYear().toString().slice(-v.length)
+    });
+})(new Date(1490486400000), 'yyyy-MM-dd')
+
+
+
 */
