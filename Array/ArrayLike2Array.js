47a53e0ecebb2bb26f8e848d3b3534c3ad563d44
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-04-09 14:55:52 +0200

    Update ArrayLike2Array.js

--- Array/ArrayLike2Array.js
+++ Array/ArrayLike2Array.js
@@ -10,3 +10,21 @@ function ArrayLike2Array(m){
 /*
 function ArrayLike2Array(m){let i;const a=m, b=Array(i=a.length);--i;while(i>-1){b[i]=a[i];--i};return b}
 */
+
+/*
+nota
+
+// Using for..of
+for(var entry of list.entries()) { 
+  console.log(entry);
+}
+ 
+results in:
+
+Array [ 0, ---]
+Array [ 1,---- ]
+Array [ 2, --- ]
+
+
+
+*/
