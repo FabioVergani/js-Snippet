6d2ad27e4eba30c503c615f2b068d88ef8473c1b
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-05-25 16:52:51 +0200

    Update blinkIt.js

--- blinkIt.js
+++ blinkIt.js
@@ -1,8 +1,9 @@
-	//#blinkIt(selector,times,delay)
+	//#blinkIt(node,times,delay)
 	function blinkIt(a,b,c){
 		setTimeout(function(){
-			var e=query(a), x=e.style, i=1, l=b*2, g=creaIntervallo(function(){
-				var s,o=x,v='visible';if(i<l){s=o.visibility!==v?v:'hidden';++i}else{eliminaIntervallo(g)};o.visibility=s
-			},7E2)
-		},c||1E3);
+			var x=a.style, i=1, l=b*2, g=creaIntervallo(function(){
+				var s,o=x,v='visible';if(i<l){s=o.visibility!==v?v:'hidden';++i}else{eliminaIntervallo(g)};
+				o.visibility=s;
+			},7E2);
+		},c);
 	}
