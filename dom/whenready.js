1fb1d562f4ea625f8d4d78cc161f440c8093cb23
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-06-04 14:46:27 +0200

    Update whenready.js

--- dom/whenready.js
+++ dom/whenready.js
@@ -3,3 +3,12 @@ function whenready(a){
  const d=document,e='DOMContentLoaded',f=a;
  if(d.readyState!=='complete'){d.addEventListener(e,b)}else{f()}
 }
+
+
+
+(function(w){
+ function a(x){console.dir(x.target)}
+ function b(x){a(x);d.removeEventListener(e,b)}
+ const d=w.document,e='DOMContentLoaded';
+ if(d.readyState!=='complete'){d.addEventListener(e,b)}else{a({target:d})}
+})(window);
