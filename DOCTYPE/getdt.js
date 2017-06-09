b72cb3fef444ab4001caa91d7b32c32f5bf1759b
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-06-09 15:07:25 +0200

    Create getdt.js

--- DOCTYPE/getdt.js
+++ DOCTYPE/getdt.js
@@ -1,6 +1,6 @@
 function dtOf(x){
- function f(a,b){return a?b+'\u0020"'+a+'"':''}
- const t=x.doctype;
- return '<!DOCTYPE\u0020'+t.name+f(t.publicId,'\u0020PUBLIC')+f(t.systemId,'')+'>';
+ function f(a,b){return a?s+b+'"'+a+'"':''}
+ const t=x.doctype,s='\u0020';
+ return ['<!DOCTYPE',s,t.name,f(t.publicId,'PUBLIC'+s),f(t.systemId,''),'>'].join('');
 }
 //console.info(dtOf(document))
