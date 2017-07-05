2511b57648f82345b782778dff5bf17f490f906b
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-07-05 14:30:42 +0200

    Update TreeWalkerTemplate-1.js

--- TreeWalker/TreeWalkerTemplate-1.js
+++ TreeWalker/TreeWalkerTemplate-1.js
@@ -37,4 +37,23 @@ If no such node exists, returns null and the current node is not changed.
 1:NodeFilter.FILTER_ACCEPT
 2:NodeFilter.FILTER_REJECT (For TreeWalker, child nodes are also rejected. For NodeIterator, this flag is synonymous with FILTER_SKIP.)
 3:NodeFilter.FILTER_SKIP (The children of skipped nodes are still considered. This is treated as "skip this node but not its children".)
+
+whatToShow
+Read only. Returns an unsigned long being a bitmask made of constants describing the types of Node that must to be presented. 
+Non-matching nodes are skipped, but their children may be included, if relevant. 
+The possible values are:
+	-1					NodeFilter.SHOW_ALL
+	1					NodeFilter.SHOW_ELEMENT
+	4					NodeFilter.SHOW_TEXT	hows Text nodes
+	64				NodeFilter.SHOW_PROCESSING_INSTRUCTION	
+	128			NodeFilter.SHOW_COMMENT
+	256			NodeFilter.SHOW_DOCUMENT
+	512			NodeFilter.SHOW_DOCUMENT_TYPE
+	1024		NodeFilter.SHOW_DOCUMENT_FRAGMENT
+obsolete:
+	2					NodeFilter.SHOW_ATTRIBUTE
+	8					NodeFilter.SHOW_CDATA_SECTION
+	16				NodeFilter.SHOW_ENTITY_REFERENCE
+	32				NodeFilter.SHOW_ENTITY
+	2048		NodeFilter.SHOW_NOTATION 
 */
