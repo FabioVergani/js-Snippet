1f5366294a62d1190baadc082e2e0dc2dd652164
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-07-21 11:35:06 +0200

    Update isFiniteNumber.js

--- Number/isFiniteNumber.js
+++ Number/isFiniteNumber.js
@@ -6,6 +6,8 @@ isNaN(x)
 +(-1)===-1//true
 +(-Infinity)===-Infinity//true
 */
-isFiniteNumber||(isFiniteNumber=function(x){
-return +x===x  && x!==Infinity && x!==-Infinity
-});
+
+//isFiniteNumber=function(x){return +x===x  && x!==Infinity && x!==-Infinity});
+
+const isFiniteNumber=x=>x===x  && x!==Infinity && x!==-Infinity;
+isFiniteNumber(1);
