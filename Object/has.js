d61e87e1553fd1bc607878ff8546036b0480f44d
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-07-22 12:47:01 +0200

    Update has.js

--- Object/has.js
+++ Object/has.js
@@ -1,7 +1,5 @@
-function has(o,p){
-	var e,x=p in o && (e=o[p]);
-	return {value:x,valueOf:function(){return this.value},refer:e};
-}
+function has(o,p){var e=p in o;return {value:e && (e=o[p]) && true,refer:e,valueOf:function(){return this.value}}}
+
 
 console.log(has({a:1},'a')?true:false)//true
 console.log(has({a:1},'a').refer)//1
