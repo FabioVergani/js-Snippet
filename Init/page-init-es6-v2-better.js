265e143529d8025696eea758bd5dee57f7494ca7
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-07-25 18:14:44 +0200

    Update page-init-es6-v2-better.js

--- Init/page-init-es6-v2-better.js
+++ Init/page-init-es6-v2-better.js
@@ -1,5 +1,6 @@
 (function(w){
-	var o=w.Element.prototype, p='remove', h=p in o, d=o[p];
+	var o=w.Element.prototype, p='remove', h=p in o, d=o[p], k='js-no';
+
 	if(h!==true||!d){
 		o[p]=function(){
 			var e=this,x=e.parentNode;
@@ -8,23 +9,37 @@
 			}
 		}
 	};
-	h=(p='head') in (d=w.document);
-	if(h=h?d[p]:(d[p]=d.getElementsByTagName(p)[0])){
-		o=d.getElementById('init')
-		p=o.getAttribute('src');
-		o.remove();
-		p=p.substring(0,p.lastIndexOf('/'))+'/get/page';
-		try{
-			new w.Function('(a=0)=>a');
-			p+='-es6'
-		}finally{
+	h='js-init';
+	if(o=(d=w.document).getElementById('page')){
+		p='classList';
+		if(p in o){
+			o[p].replace(k,h)
+		}else{
+			o[p='className']=o[p].replace(k,h)
+		};
+		if(h=((p='head') in d)?d[p]:(d[p]=d.getElementsByTagName(p)[0])){
+			o=d.getElementById('init');
+			p=o.getAttribute('src');
+			o.remove();
+			p=p.substring(0,p.lastIndexOf('/'))+'/get/page';
 			try{
-				o=d.createElement('script');
-				d.createDocumentFragment().appendChild(o)
+				new w.Function('(a=0)=>a');
+				p+='-es6'
 			}finally{
-				o.src=p+'-dev.js';//-dev
-				h.appendChild(o)	
+				try{
+					o=d.createElement('script');
+					d.createDocumentFragment().appendChild(o)
+				}finally{
+					o.src=p+'-dev.js';//-dev
+					h.appendChild(o)	
+				}
 			}
 		}
-	}
+	}else{
+		p='documentElement';
+		o=d[p]||(d[p]=d.getElementsByTagName('html')[0]);
+		p=o.className.replace(k,h);
+		o.className=p+' dom-unknown';
+	};
+
 })(window);
