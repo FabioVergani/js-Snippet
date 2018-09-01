12ef4fe72bbb00b75dced68372862594891bcbb6
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-09-01 23:29:18 +0200

    Update dos-t.js

--- date/dos-t.js
+++ date/dos-t.js
@@ -1,3 +1,5 @@
 const o=new Date();
 let t=(((o.getUTCHours()<<6)|o.getUTCMinutes())<<5)|o.getUTCSeconds()/2;
 console.info('dosTime',t);
+t=((((o.getUTCFullYear()-1980)<<4)|(o.getUTCMonth()+1))<<5)|o.getUTCDate();
+console.info('dosDate',t);
