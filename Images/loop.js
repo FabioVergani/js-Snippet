92c1fc299a00c994df7a9fdb4bf14b562315161d
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-09-07 15:44:10 +0200

    Update loop.js

--- Images/loop.js
+++ Images/loop.js
@@ -1,7 +1,31 @@
 (w=>{
 	const d=w.document, images=[...d.images];
+	let i=images.length;
+	while(i!==0){
+		const image=images[--i];
+		console.log(i,image.src)
+	}
+})(window);
+
+
+/*
+(w=>{
+	const d=w.document, images=[...d.images];
+	while(images.length!==0){
+		const image=images.pop();
+		console.log(image.src)
+	}
+})(window);
+
+
+//OR
+
+
+(w=>{
+	const d=w.document, images=[...d.images];
 	while(images.length){
 		const image=images.shift();
 		console.log(image.src)
 	}
 })(window);
+*/
