56535b19fb5e56ace847192458d7cbeb508617af
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-11-10 16:18:03 +0100

    Create get url inside url .js

---
+++ String/get url inside url .js
@@ -0,0 +1,11 @@
+  // get url inside url("...")
+  var reURL = /url\((['"])?(.*?)\1\)/gi;
+  var matches = reURL.exec( style.backgroundImage );
+  while ( matches !== null ) {
+    var url = matches && matches[2];
+    if ( url ) {
+      //
+    }
+    matches = reURL.exec( style.backgroundImage );
+  }
+};
