57fb07a30f03bc469d0080701f809cb1d94afcab
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-11-14 16:40:20 +0100

    Update deviceMobile.js

--- RegEx/deviceMobile.js
+++ RegEx/deviceMobile.js
@@ -1,5 +1,5 @@
 const deviceMobile=(n=>{
 			let v=false;
-			if(n && (v=n.userAgent) && v.length!==0){v=/(?:\b)(?:((?:Android|Chrome)(?:.+)Mobile)|iP(?:hone|od|ad)|Mobile(?:.+)Firefox|Opera Mini|Windows\s*(?:\bARM)?\b|B(?:lackBerry|B10))/i.test(v)};
+			if(n && (v=n.userAgent) && v.length!==0){v=/(?:\b)(?:((?:Android|Chrome)(?:.+)Mobile)|iP(?:hone|od|ad)|Mobile(?:.+)Firefox|Opera Mini|Windows\sARM?\b|B(?:lackBerry|B10))/i.test(v)};
 			return v
 })(navigator),
