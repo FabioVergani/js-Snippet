fa9f1b726c48db94bd286980ecc3ba1098b5000a
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-11-14 22:43:43 +0100

    Update deviceMobile.js

--- RegEx/deviceMobile.js
+++ RegEx/deviceMobile.js
@@ -3,3 +3,11 @@ const deviceMobile=(n=>{
 			if(n && (v=n.userAgent) && v.length!==0){v=/(?:\b)(?:((?:Android|Chrome)(?:.+)Mobile)|iP(?:hone|od|ad)|Mobile(?:.+)Firefox|Opera Mini|Windows\bARM?\b|B(?:lackBerry|B10))/i.test(v)};
 			return v
 })(navigator),
+
+/*
+			let v=w.navigator;
+			if(v && (v=v.userAgent) && v.length!==0 && /(?:\b)(?:((?:Android|Chrome)(?:.+)Mobile)|iP(?:hone|od|ad)|Mobile(?:.+)Firefox|Opera Mini|Windows\bARM?\b|B(?:lackBerry|B10))/i.test(v)){
+				d.addEventListener('mousewheel',o=>{if(o.ctrlKey===true){o.preventDefault();c.info('zoom prevented!')}})
+			}
+
+*/
