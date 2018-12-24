7337b7034b78cfb3c0c20cbbd4b73d7f82a101f9
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-12-24 21:24:41 +0100

    Update querySelectorAll_iterate1.js

--- loop/querySelectorAll_iterate1.js
+++ loop/querySelectorAll_iterate1.js
@@ -1,9 +1,23 @@
  const m = d.querySelectorAll('');
  //console.dir(m);
 
+	console.dir(m);
+	let i=m.length;
+	if(i!==0){
+		const a=Array(i);
+		i=0;
+		for(const e of list){
+			a[i]=e;
+			++i;
+		};
+		console.dir(a)
+	};
+
+
+/*
  let n,i=m.length;
  while(i){
 	n=m[i];
 	console.dir(n);
-	m.length=--i;
- };
+	--i;
+ };*/
