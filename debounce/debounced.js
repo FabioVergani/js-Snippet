9edc19c8c49904d15e1b7f3b2b9bb3f06f7db446
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-01-02 13:25:34 +0100

    Create debounced.js

---
+++ debounce/debounced.js
@@ -0,0 +1,12 @@
+const debounced=(delay, fn)=>{
+  let timerId;
+  return function (...args) {
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+    timerId = setTimeout(() => {
+      fn(...args);
+      timerId = null;
+    }, delay);
+  }
+}
