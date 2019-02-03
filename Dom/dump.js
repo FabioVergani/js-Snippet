18084b2d48b5d68728f28374251f42b74e4b57b5
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-02-03 15:01:30 +0100

    Update dump.js

--- Dom/dump.js
+++ Dom/dump.js
@@ -10,51 +10,59 @@
 			}else{
 				f(d)
 			}
-		};
+		},
+		matrix=[];
 		//
 		ready(w1,d1=>{
-			const frag=d1.createDocumentFragment(),
-			walk=(branch,depth,nth,e)=>{
-				if(0!==e.offsetHeight){
-					switch(e.nodeType){
-						case 3:
-							branch.appendChild(d1.createTextNode(e.nodeValue));//todo:skip blank
-							break
-						case 1:
-							branch=branch.appendChild(d1.createElement(e.tagName));
-							break
-					};
-					//console.log('\t'.repeat(depth),nth,e.tagName||e.nodeValue);
-					if((e=e.firstChild) && 1===e.nodeType){
-						++depth
-					};
-					while(null!==e){
-						walk(branch,depth,nth,e);
-						if(null!==(e=e.nextSibling) && 1===e.nodeType){
-							++nth
+			if(d1.body){
+				const w1=d1.defaultView,
+				frag=d1.createDocumentFragment(),
+				walk=(branch,depth,nth,e)=>{
+					if(0!==e.offsetHeight){
+						switch(e.nodeType){
+							case 3:
+								branch.appendChild(d1.createTextNode(e.nodeValue));//todo:skip blank
+								break
+							case 1:
+								branch=branch.appendChild(d1.createElement(e.tagName));
+								const m=matrix;
+								m[m.length]=[[e,w1.getComputedStyle(e)],[branch,null]];
+								break
+						};
+						//console.log('\t'.repeat(depth),nth,e.tagName||e.nodeValue);
+						if((e=e.firstChild) && 1===e.nodeType){
+							++depth
+						};
+						while(null!==e){
+							walk(branch,depth,nth,e);
+							if(null!==(e=e.nextSibling) && 1===e.nodeType){
+								++nth
+							}
 						}
 					}
-				}
-			};
-			//
-			walk(frag,0,1,d1.body);
-			//
-			ready(d1.defaultView.open(),d2=>{
-				let x=d1,t=x.title;
-				d2.title=t||x.URL;
-				if(x=x.doctype){
-					x=x.cloneNode()
-				}else{
-					x=d2.implementation.createDocumentType('html',null,null)
 				};
-				if(t=d2.doctype){
-					d2.replaceChild(t,x)
-				}else{
-					d2.insertBefore(x,d2.childNodes[0])
-				};
-				if(x=frag.firstElementChild){
-					d2.body.replaceWith(x)
-				}
-			})
+				//
+				walk(frag,0,1,d1.documentElement);
+				//
+				console.dir(matrix);
+				//
+				ready(w1.open(),d2=>{
+					let x=d1;
+					if(x=x.doctype){
+						x=x.cloneNode()
+					}else{
+						x=d2.implementation.createDocumentType('html',null,null)
+					};
+					const t=d2.doctype;
+					if(t){
+						d2.replaceChild(t,x)
+					}else{
+						d2.insertBefore(x,d2.childNodes[0])
+					};
+					if(x=frag.firstElementChild){
+						d2.documentElement.replaceWith(x)
+					}
+				})
+			}
 		})
 	})(window);
