436239c41ab571da0ba0359d590dbe1ee09d2a97
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-02-12 18:51:30 +0100

    Update Tweak for loops.js

--- codegolf/Tweak for loops.js
+++ codegolf/Tweak for loops.js
@@ -8,4 +8,8 @@ for(p;q;r)   // after
   
 // Compressed iterative loop
 i=10;while(i--);  // before
-for(i=10;i--;);   // after
+for(i=10;i--;);   // after.
+
+//Variable init inside loop
+for(j=0;p=1,j<n;j++) // before
+for(j=0;p=j<n;j++)   // after
