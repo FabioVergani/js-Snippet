65e326a8a269067514e6c2ef0cb1f5655e66c494
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-02-12 19:22:03 +0100

    Create produces shorter names with no conflicts .js

---
+++ codegolf/produces shorter names with no conflicts .js
@@ -0,0 +1,4 @@
+    a=65;
+    for(k in g = z.getContext('experimental-webgl'))
+        t[String.fromCharCode(a)] = t[k] = g[k].bind && a++ && g[k].bind(g);
+(this starts with uppercase "A", char code 65)
