3ab25750b0289817855f54145810ce4e807c6685
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-02-12 18:46:39 +0100

    Update keywords-repeating.js

--- codegolf/keywords-repeating.js
+++ codegolf/keywords-repeating.js
@@ -10,3 +10,10 @@ This code has many "function" keywords repeating. What if you could replace them
 Here's how I would do it :
 
 eval('a=F(){/*codehere*/};b=F(){/*codehere*/};c=F(){/*codehere*/};/*...*/z=F(){/*codehere*/};'.replace(/function/g,'F'));
+
+
+//
+
+var a = "eval(''.replace(/function/g,'F'))".length,
+    b = ('function'.length-'F'.length)*4;
+alert("you should" + (a<b?"":" NOT") + " use this technique!");
