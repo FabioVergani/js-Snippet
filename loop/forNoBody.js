fa9afad9a78f5c84c250851395142b0c9dcaa540
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-02-17 15:34:16 +0100

    Update forNoBody.js

--- loop/forNoBody.js
+++ loop/forNoBody.js
@@ -5,3 +5,10 @@ function fn(x){return x<5}
 for(let m=[1,2,3,4,5,6,7,8],i=m.length;0!==i;(--i,console.log(i,m[i]+'<5',fn(m[i]))));
 
 
+function fn(x){
+  console.log(x+'<5',x<5)
+  return x<5
+}
+
+for(let m=[1,2,3,4,5,6,7,8],i=m.length;0!==i;i=fn(m[--i])?0:i);
+
