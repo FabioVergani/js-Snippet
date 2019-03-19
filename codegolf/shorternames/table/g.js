70587250fa8bf2077bb34d24d64c3f3cf08f4a54
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-03-19 14:19:27 +0100

    Create g.js

---
+++ codegolf/shorternames/table/g.js
@@ -0,0 +1,30 @@
+const d=document;
+//
+const g=d.createElement('canvas').getContext('experimental-webgl');
+//
+d.open();
+j=0;
+for(i in g){//orderin NOT uaranteed
+	d.writeln((i[0]+i[6])+' | '+(i[9]?[0,6]:[0,7,8]).map(n=>i[n]||'').join('')+': '+i)
+};
+d.close();
+//
+/*
+vA | vA: vertexAttribPointer
+eV | eV: enableVertexAttribArray
+cB | cB: createBuffer
+bD | bD: bufferData
+cP | cP: createProgram
+sS | sS: shaderSource
+cS | cS: createShader
+ce | ce: compileShader
+aS | aS: attachShader
+lo | lo: linkProgram
+ug | ug: useProgram
+um | u1f: uniform1f
+um | u2f: uniform2f
+um | u4f: uniform4f
+*/
+
+
+
