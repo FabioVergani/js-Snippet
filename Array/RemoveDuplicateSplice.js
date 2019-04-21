b42a6e373eb785cafb72b3e3b865d88a0ca0f1be
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-04-21 23:42:06 +0200

    Update RemoveDuplicateSplice.js

--- Array/RemoveDuplicateSplice.js
+++ Array/RemoveDuplicateSplice.js
@@ -1,11 +1,16 @@
-		const m=[];
 
-		let l=m.length,i=1;
-		while(i<l){
-		  if(i!==m.indexOf(m[i])){
-			 m.splice(i,1);
-			 --l
-		  }else{
-			 ++i
-		  }
+	uniques=m=>{
+		let l=m.length;
+		if(0!==l){
+			let i=1;
+			while(i<l){
+			  if(i!==m.indexOf(m[i])){
+				 m.splice(i,1);
+				 --l
+			  }else{
+				 ++i
+			  }
+			}
 		};
+		return m
+	}
