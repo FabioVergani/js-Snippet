a7591ee5845f1f54caa2417eb4abd007f025d4d8
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-04-22 09:26:09 +0200

    Update RemoveDuplicateSplice.js

--- Array/RemoveDuplicateSplice.js
+++ Array/RemoveDuplicateSplice.js
@@ -1,4 +1,21 @@
+const uniques=m=>{
+	if(null!==m){//<
+		let i=m.length;
+		while(0!==i){
+		if(i!==m.indexOf(m[i])){
+			m.splice(i,1);
+			--i
+		};
+		--i
+		}
+	}
+};
 
+//uniques([1,2,3,3,'a',4,5,'a',6,7,8,9,10,'a',11,12,'a',13]);
+
+
+
+/*
 	uniques=m=>{
 		let l=m.length;
 		if(0!==l){
@@ -14,3 +31,4 @@
 		};
 		return m
 	}
+*/
