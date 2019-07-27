1c528980a33088242ca308fbd9102fd68a829806
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-07-27 16:02:43 +0200

    Create LoopObjectEntries.js

---
+++ Object/LoopObjectEntries.js
@@ -0,0 +1,22 @@
+for(const [key,val] of Object.entries(fruits)){
+	console.log(`pair ${key} ${val}`)
+}
+
+/*
+const entries = Object.entries(fruits)
+console.log(entries)
+// [
+//   [apple, 28],
+//   [orange, 17],
+//   [pear, 54]
+// ]
+
+for (const [fruit, count] of entries) {
+  console.log(`There are ${count} ${fruit}s`)
+}
+
+// Result
+// There are 28 apples
+// There are 17 oranges
+// There are 54 pears
+*/
