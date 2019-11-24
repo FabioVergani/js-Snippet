354c6fd8131c7beab4ad76f2c00e0c491601dbd5
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-11-24 19:53:36 +0100

    Update closure-compiler.js

--- Fetch/closure-compiler.js
+++ Fetch/closure-compiler.js
@@ -1,4 +1,16 @@
-		fetch('https://closure-compiler.appspot.com/compile',{
+		$minify.onclick=event=>{
+			const s=$textarea.value.trim();
+			if(event.currentTarget.disabled=s.length?true:false){
+				fetch('https://closure-compiler.appspot.com/compile',{
+					headers:{'Content-Type':'application/x-www-form-urlencoded'},
+					body:['js_code=','&output_info=errors&output_info=compiled_code&output_format=json'].join(encodeURIComponent(s)),
+					method:'POST',
+				}).then(resp=>resp.json()).then(handleClosureCompilerResponse)
+			}
+		};		
+
+/*
+fetch('https://closure-compiler.appspot.com/compile',{
 			headers:{'Content-Type':'application/x-www-form-urlencoded'},
 			body:[
 				['js_code',6666],
@@ -13,3 +25,4 @@
 			const v=resp.json();
 			console.dir(v.compiledCode)
 		})
+*/
