1290d0696b68cd4f917df280903cc5b615a9b02f
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-12-15 17:47:08 +0100

    Update loadrequired.js

--- Promise/loadrequired.js
+++ Promise/loadrequired.js
@@ -6,25 +6,26 @@
 	d=w.document,
 	frag=d.createDocumentFragment(),
 	nest=x=>frag.appendChild(d.createElement(x)),
-	required=[
-		'a.js',
-		'b.js',
-		'c.js',
-		'd.js',
-		'e.js',
+	js=[
+		'a',
+		'b',
+		'c',
+		'd',
+		'e',
 	];
 	/*
 		...
 	*/
-	required.forEach((v,i,m)=>{
+	js.forEach((v,i,m)=>{
 		const e=nest('link');
 		e.rel='preload';
 		e.as='script';
-		e.href='./res/script/'+v;
+		e.href=['./res/script/','.js'].join(v);
 		m[i]=new Promise(f=>e.onload=()=>f(e))
 	});
 	d.head.appendChild(frag);
-	Promise.all(required).then(m=>{
+	Promise.all(js).then(m=>{
+		js.length=0;
 		let v;
 		while(v=m.shift()){
 			const e=nest('script');
