243c97139e1463eae4db84d1bdbc98a590bd6a85
Fabio Vergani <fabio.vergani.1975@gmail.com>
2020-03-06 13:51:03 +0100

    Update areEqualArrays.js

--- Array/areEqualArrays.js
+++ Array/areEqualArrays.js
@@ -1,66 +1,58 @@
 const bothAreObjects = (a, b) => {
-  const x = typeof a;
-  return x !== typeof b ? false : "object" === x;
+    const type = typeof a;
+    return type !== typeof b ? false : 'object' === type;
 };
 
 const areEqualArrays = (a, b) => {
-  if (a.length !== b.length) {
-    return false;
-  } else {
-    const foundiff = a.some((aVal, i, m) => {
-      const bVal = b[i];
-      if (aVal !== bVal) {
-        const areComparable = bothAreObjects;
-        if (areComparable(aVal, bVal)) {
-          //console.log("compare val", aVal, bVal);
-          const entriesOf = Object.entries,
-            aValEntries = entriesOf(aVal),
-            bValEntries = entriesOf(bVal);
-          let i = aValEntries.length;
-          if (i !== bValEntries.length) {
-            return true;
-          } else {
-            while (i > 0) {
-              const iA = aValEntries[--i],
-                iB = bValEntries[i];
-              //console.log(iA, iB);
-              if (iA[0] !== iB[0]) {
-                return true; //key are diff
-              } else {
-                const iAv = iA[1],
-                  iBv = iB[1];
-                if (iAv !== iBv) {
-                  if (areComparable(iAv, iBv)) {
-                    //console.log("compare subVal", iAv, iBv);
-                    return (
-                      entriesOf(iAv).toString() !== entriesOf(iBv).toString()
-                    );
-                  } else {
-                    return true;
-                  }
-                } else {
-                  return false;
-                }
-              }
-            }
-          }
-        } else {
-          return true;
-        }
-      } else {
+    if (a.length !== b.length) {
         return false;
-      }
-    });
-    return true !== foundiff;
-  }
+    } else {
+        return (
+            true !==
+            a.some((aVal, index) => {
+                const bVal = b[index];
+                if (aVal !== bVal) {
+                    const areComparable = bothAreObjects;
+                    if (areComparable(aVal, bVal)) {
+                        //console.log("compare val", aVal, bVal);
+                        let i = Object.entries;
+                        const aValEntries = i(aVal),
+                            bValEntries = i(bVal);
+                        i = aValEntries.length;
+                        if (i !== bValEntries.length) {
+                            return true;
+                        } else {
+                            while (i > 0) {
+                                --i;
+                                const iA = aValEntries[i],
+                                    iB = bValEntries[i];
+                                //console.log(iA, iB);
+                                if (iA[0] !== iB[0]) {
+                                    return true; //key are diff
+                                } else {
+                                    const iAv = iA[1],
+                                        iBv = iB[1];
+                                    if (iAv !== iBv) {
+                                        if (areComparable(iAv, iBv)) {
+                                            //console.log("compare subVal", iAv, iBv);
+                                            const f = JSON.stringify;
+                                            return f(iAv) !== f(iBv);
+                                        } else {
+                                            return true;
+                                        }
+                                    } else {
+                                        return false;
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
+            })
+        );
+    }
 };
-//=========================================
-
-const x = { q: 1 };
-const oA = { p: x };
-const oB = { p: { q: 1 } };
-
-const a = [1, 2, oA];
-const b = [1, 2, oB];
-
-console.log(2, areEqualArrays(a, b)); // true
