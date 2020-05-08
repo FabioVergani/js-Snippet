97a38b83e7e6115c90353c30e78a5d1730979968
Fabio Vergani <fabio.vergani.1975@gmail.com>
2020-05-08 20:04:35 +0200

    Update randInt.js

--- Number/randInt.js
+++ Number/randInt.js
@@ -2,3 +2,8 @@ const randInt=(a,b)=>{//Inclusive min,max
 	const M=Math,f=M.floor,i=M.ceil(a);
 	return f(M.random()*(f(b)-i+1))+ i
 }
+
+
+function randomInt(max,min=0){
+    return (Math.random() * (max - min) + min) << 0 
+}
