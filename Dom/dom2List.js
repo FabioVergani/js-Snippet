aa6ff555327c30b142af1100aba5d93d9f4629bd
Fabio Vergani <fabio.vergani.1975@gmail.com>
2021-08-03 20:21:19 +0200

    Create dom2List.js

---
+++ Dom/dom2List.js
@@ -0,0 +1,16 @@
+const dom2List = root => {
+  const 
+    d = root.ownerDocument,
+		e = (a,b) => a.appendChild(d.createElement(b)),
+		f = (node, nest) => {
+			const ul = e(nest, 'ul');
+			ul.appendChild(d.createTextNode(node.nodeName));
+			for (const child of node.children) {
+				f(child, e(ul, 'li'));
+			}
+			return ul;
+		};
+	return f(root, d.createDocumentFragment());
+};
+
+//document.body.appendChild(dom2List(document.firstElementChild));
