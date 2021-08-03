c0c36ab562c3ec29ec6b1bd9299ebd411fa8574b
Fabio Vergani <fabio.vergani.1975@gmail.com>
2021-08-03 20:26:48 +0200

    Update dom2List.js

--- Dom/dom2List.js
+++ Dom/dom2List.js
@@ -14,3 +14,27 @@ const dom2List = root => {
 };
 
 //document.body.appendChild(dom2List(document.firstElementChild));
+
+/*
+const dom2List = (root, callback) => {
+	const 
+	        d = root.ownerDocument,
+		e = (a, b) => a.appendChild(d.createElement(b)),
+		f = (node, nest) => {
+			const ul = e(nest, 'ul');
+			callback(ul.appendChild(d.createTextNode(node.nodeName)));
+			for (const child of node.children) {
+				f(child, e(ul, 'li'));
+			}
+			return ul;
+		};
+	return f(root, d.createDocumentFragment());
+};
+
+document.body.appendChild(
+    dom2List(
+	document.firstElementChild, 
+	tagName => {console.log(tagName);}
+    )
+);
+*/
