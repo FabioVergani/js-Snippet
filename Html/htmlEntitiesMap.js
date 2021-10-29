3d341c32dda129b577bbb69a8dcc81b90fac5862
Fabio Vergani <fabio.vergani.1975@gmail.com>
2021-10-29 22:03:28 +0200

    Update htmlEntitiesMap.js

--- Html/htmlEntitiesMap.js
+++ Html/htmlEntitiesMap.js
@@ -1,4 +1,4 @@
-  const htmlEntitiesMap = new Map([
+  const htmlEntitiesMap1 = new Map([
     ['&','&amp;'],
     ['<','&lt;'],
     ['>','&gt;'],
@@ -6,5 +6,16 @@
     ["'",'&apos;']
   ]);
 
-  const htmlEntitiesRE = /([&<>"'])/g;
-  console.log(``.replaceAll(htmlEntitiesRE, match => htmlEntitiesMap.get(match)));
+  const htmlEntitiesRE1 = /([&<>"'])/g;
+  console.log(``.replaceAll(htmlEntitiesRE1, match => htmlEntitiesMap1.get(match)));
+
+  const htmlEntitiesMap2 = new Map([
+    ['&amp;','&'],
+    ['&lt;','<'],
+    ['&gt;','>'],
+    ['&quot;','"'],
+    ['&apos;',"'"]
+  ]);
+
+  const htmlEntitiesRE2 = /(&(?:a(?:mp|pos)|(?:l|g|quo)t))/g;
+  str.replace(htmlEntitiesRE2, match => htmlEntitiesMap2.get(match));
