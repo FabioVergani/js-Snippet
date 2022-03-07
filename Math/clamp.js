85de86241ec9fe2817902b990da8b332b4cb1856
Fabio Vergani <fabio.vergani.1975@gmail.com>
2022-03-07 20:17:17 +0100

    Update clamp.js

--- Math/clamp.js
+++ Math/clamp.js
@@ -4,3 +4,5 @@ const clamp = ( n, a, b ) => ( n <= a ? a : n >= b ? b : n );
 
 //Restrict Number to Range within min and max
 const clamp2 = (min, val, max) => Math.max(min, Math.min(val, max));
+
+const clamp3 = (num, min, max) => Math.min(Math.max(num, min), max);
