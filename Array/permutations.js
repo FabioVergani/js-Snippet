b16c057427ef9ea6334c1140b448332ccf52dbc1
Fabio Vergani <fabio.vergani.1975@gmail.com>
2022-04-27 19:57:02 +0200

    Update permutations.js

--- Array/permutations.js
+++ Array/permutations.js
@@ -17,3 +17,12 @@ const permutations = value => {
 };
 
 console.log(permutations('ab')); 
+
+
+//----
+
+const cartesianProduct = (a, b) =>
+  a.reduce((p, x) => [...p, ...b.map(y => [x, y])], []);
+
+cartesianProduct(['x', 'y'], [1, 2]);
+// [['x', 1], ['x', 2], ['y', 1], ['y', 2]]
