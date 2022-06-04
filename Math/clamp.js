06461263a92e6ba44f83a64fa1a002100dd1cb19
Fabio Vergani <fabio.vergani.1975@gmail.com>
2022-06-04 11:08:23 +0200

    Update clamp.js

--- Math/clamp.js
+++ Math/clamp.js
@@ -5,9 +5,19 @@ const clamp = ( n, a, b ) => ( n <= a ? a : n >= b ? b : n );
 
 //Restrict Number to Range within min and max
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
-*/
+
 
 const clamp = (n, min, max) => {
 	const a = min ?? -Infinity, b = max ?? Infinity;
+	return n <= a ? a : n >= b ? b : n;
+};
+
+*/
+
+// prettier-ignore
+const clamp = (n, params) => {
+	const i = Infinity,
+	a = params.min ?? -i,
+	b = params.max ?? i;
 	return n <= a ? a : n >= b ? b : n;
 };
