35d1d99641e4b5c9110b8dce3519169ac6f0ea9b
Fabio Vergani <fabio.vergani.1975@gmail.com>
2022-06-05 14:03:28 +0200

    Update eliminateDuplicatesFromString.js

--- RegEx/eliminateDuplicatesFromString.js
+++ RegEx/eliminateDuplicatesFromString.js
@@ -1,1 +1,21 @@
 const eliminateDuplicatesFromString = str => str.replace(/(\b\S.+\b)(?=.*\1)/g, "").trim();
+
+/*
+https://www.measurethat.net/Benchmarks/Show/19182/1/eliminate-duplicates-from-an-array-of-strings
+const str = 'Hello world nice to meet you hello again world';
+
+// 97,988 ops/sec
+console.log('unique words:', [...new Set(str.toLowerCase().split(/\s+/))]);
+
+// 96,867 ops/sec
+const words = {};
+for ( const word of str.toLowerCase().split(/\s+/)) {
+ words[word] = word;
+}
+console.log('unique words:', Object.keys(words));
+
+// 63,593 ops/sec
+console.log('unique words:',str.toLowerCase().split(/\s+/).filter((v, i, m) =>  i === m.indexOf(v)));
+
+console.log('unique words:',str.replace(/(\b\S.+\b)(?=.*\1)/g, "").trim());
+*/
